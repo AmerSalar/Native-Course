22336ad1ac2140c7df6d1cb03d1e8bf7fc5fa116
Ameer Salar <amersalar108@gmail.com>
2026-04-08 23:09:37 +0300

    Tab Navigation continued

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -2,15 +2,18 @@ import { Tabs } from "expo-router";
 import { tabs } from "../../consts/data";
 import { Image, View } from "react-native";
 import clsx from "clsx";
-
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { components, colors } from "../../consts/theme";
+const tabBar = components.tabBar;
 const TabLayout = () => {
+  const insets = useSafeAreaInsets();
   const TabIcon = ({ focused, icon }) => {
     return (
       <View className={"size-12 items-center justify-center"}>
         <View
           className={clsx(
-            "size-12 items-center justify-center rounded-full bg-transparent",
-            focused && "bg-red-400",
+            "size-12 items-center justify-center rounded-full bg-[#0000]",
+            focused && "bg-[#ea7a53]",
           )}
         >
           <Image source={icon} className={"size-6"} />
@@ -24,10 +27,22 @@ const TabLayout = () => {
         headerShown: false,
         tabBarShowLabel: false,
         tabBarStyle: {
-          backgroundColor: "transparent",
+          position: "absolute",
+          bottom: Math.max(insets.bottom, tabBar.horizontalInset),
+          backgroundColor: colors.primary,
+          marginHorizontal: tabBar.horizontalInset,
+          borderRadius: tabBar.radius,
           borderTopWidth: 0,
           elevation: 0,
-          height: 60,
+          height: tabBar.height,
+        },
+        tabBarItemStyle: {
+          paddingVertical: tabBar.height / 2 - tabBar.iconFrame / 1.6,
+        },
+        tabBarIconStyle: {
+          width: tabBar.iconFrame,
+          height: tabBar.iconFrame,
+          alignItems: "center",
         },
       }}
     >
--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,8 +1,5 @@
-import {} from "react-native";
-import "../../global.css";
 import { Text, View } from "react-native";
-import { Link } from "expo-router";
-import "../../global.css";
+
 export default function App() {
   return (
     <View className={"flex-1 justify-center items-center"}>
